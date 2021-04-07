719ab275a19b92a0000a90a20c214159763cbf37
Bart Willems <bwillems@protonmail.com>
2021-04-07 17:33:09 +0200

    use another lyrics api (this one has more results and is less down)
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -11,6 +11,8 @@ pub enum LyriekError {
     ArtistNotFound,
     #[display(fmt = "Song title not found")]
     TitleNotFound,
+    /// The API response was succesful, but there were no lyrics found
+    LyricsNotFound,
 }
 
 impl From<mpris::DBusError> for LyriekError {
@@ -28,5 +30,12 @@ impl From<mpris::FindingError> for LyriekError {
 impl<Msg> From<std::sync::mpsc::SendError<Msg>> for LyriekError {
     fn from(error: std::sync::mpsc::SendError<Msg>) -> LyriekError {
         LyriekError::Communication(format!("communication error: {}", error))
+    }
+}
+
+impl From<reqwest::Error> for LyriekError {
+    fn from(error: reqwest::Error) -> LyriekError {
+        error!("API request failed: {}", error);
+        LyriekError::LyricsNotFound
     }
 }
--- src/song.rs
+++ src/song.rs
@@ -1,14 +1,54 @@
 use std::convert::TryFrom;
-use std::error::Error;
+use std::fmt::Debug;
 
 use url::Url;
 
 use crate::errors::LyriekError;
 
-#[derive(Deserialize)]
-struct ApiResponse {
-    lyrics: String,
+#[derive(Debug, Deserialize)]
+struct ApiResponse<T: Debug> {
+    result: T,
 }
+
+#[derive(Debug, Deserialize)]
+struct ApiSearchResponse {
+    artist: String,
+    id_artist: usize,
+    track: String,
+    id_track: usize,
+    album: String,
+    id_album: usize,
+    api_lyrics: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ApiLyricsResponse {
+    artist: String,
+    id_artist: usize,
+    track: String,
+    id_track: usize,
+    album: String,
+    id_album: usize,
+    lyrics: String,
+    /// URL to this resource
+    api_lyrics: String,
+}
+
+// #[derive(Deserialize)]
+// struct ApiResponse {
+//     result: ApiResult,
+//     // lyrics: String,
+// }
+
+// #[derive(Deserialize)]
+// struct ApiResult {
+//     track: Track,
+// }
+
+// #[derive(Deserialize)]
+// struct Track {
+//     text: String,
+// }
 
 #[derive(Clone, Debug)]
 pub enum Lyrics {
@@ -71,75 +111,48 @@ impl Song {
         Song::try_from(&metadata)
     }
 
-    /// returns the url::Url to fetch the lyrics for the current song
-    /// returns an error if the url can't be parsed
-    fn get_lyrics_api_uri(&self) -> url::Url {
-        let mut url = Url::parse("https://api.lyrics.ovh/v1").expect("invalid api base url");
+    /// Try to load the lyrics from the API
+    fn search_lyrics(&mut self) -> Result<String, LyriekError> {
+        let mut url = Url::parse("https://api.happi.dev/v1/music").expect("invalid base API url");
 
-        url.path_segments_mut()
-            .expect("url can not be base")
-            .push(&self.artists)
-            .push(&self.title);
+        url.query_pairs_mut()
+            .append_pair("q", &format!("{} {}", self.artists, self.title))
+            .append_pair("limit", "1")
+            .append_pair("lyrics", "1")
+            .append_pair("type", "track")
+            // TODO: this is a free tier API key, I should replace this with a server of my own so I don't have to push this to github
+            // For anyone who reads this: Don't steal this API key. (Or do, I don't really care atm)
+            .append_pair(
+                "apikey",
+                "84cbb8oiGQIoD68KLQSscMr3jNYWwvQSFFhpq46tNfHcRQComN1lMd4d",
+            );
 
-        url
+        let mut resp: ApiResponse<Vec<ApiSearchResponse>> = reqwest::blocking::get(url)?.json()?;
+
+        let lyrics_url = resp
+            .result
+            .pop()
+            .ok_or_else(|| LyriekError::LyricsNotFound)?;
+
+        let lyrics: ApiResponse<ApiLyricsResponse> = reqwest::blocking::get(
+            lyrics_url.api_lyrics
+                + "?apikey=84cbb8oiGQIoD68KLQSscMr3jNYWwvQSFFhpq46tNfHcRQComN1lMd4d",
+        )?
+        .json()?;
+
+        Ok(lyrics.result.lyrics)
     }
 
-    /// sets the lyrics for the song
-    pub fn get_lyrics(&mut self) -> Result<(), Box<dyn Error>> {
-        let url = &self.get_lyrics_api_uri();
-
-        debug!("fetching lyrics from {}", url.as_str());
-
-        let resp: ApiResponse = reqwest::blocking::get(url.as_str())
-            .map_err(|e| {
-                error!("lyrics api error: {}", e);
+    pub fn get_lyrics(&mut self) -> Result<(), LyriekError> {
+        match self.search_lyrics() {
+            Ok(lyrics) => {
+                self.lyrics = Lyrics::Found(lyrics);
+                Ok(())
+            }
+            Err(e) => {
                 self.lyrics = Lyrics::NotFound;
-                e
-            })?
-            .json()
-            .or_else(|e| {
-                debug!("unable to fetch lyrics: {}", e);
-                self.lyrics = Lyrics::NotFound;
-                Err("lyrics not found")
-            })?;
-
-        // For some reason, this lyrics api adds too much new lines
-        self.lyrics = Lyrics::Found(resp.lyrics.replace("\n\n", "\n"));
-        Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_lyrics_api_path_ordering() {
-        let song: Song = Song {
-            title: String::from("Blackwater Park"),
-            artists: String::from("Opeth"),
-            lyrics: Lyrics::default(),
-            album: None,
-            album_art_url: None,
-            url: None,
-        };
-
-        let uri = song.get_lyrics_api_uri();
-        // This is to make sure the artist & song title aren't switched
-        assert_eq!(uri.path(), "/v1/Opeth/Blackwater%20Park");
-    }
-
-    #[test]
-    fn test_lyrics_api_url_encoding() {
-        let mut song: Song = Song::new();
-        song.artists = String::from("Slayer");
-        song.title = String::from("Metal Storm / Face the Slayer");
-
-        let uri = song.get_lyrics_api_uri();
-
-        assert_eq!(
-            uri.path(),
-            "/v1/Slayer/Metal%20Storm%20%2F%20Face%20the%20Slayer"
-        );
+                return Err(e);
+            }
+        }
     }
 }
